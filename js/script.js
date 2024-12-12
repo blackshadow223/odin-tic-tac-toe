b360796bf7a4c2cd4138e3d9db4a3b9639ad4c55
Black Shadow <mithabozdar2@gmail.com>
2024-12-12 20:27:30 +0500

    Make Board module to handle board logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
 console.log("Hello, World!");
+    const Board = (function() {
+        // Board
+        const board = [];
+
+        // Each cell can have 3 values
+        // Either empty, 'X', or 'O'
+
+        for (let i = 0; i < 3; ++i) {
+            // Create a row if not already done
+            board[i] = [];
+
+            for (let j = 0; j < 3; ++j) {
+                board[i].push("");
+            }
+        }
+
+        function getCell(row, column) {
+            if (row < 3 && column < 3) {
+                return board[row][column];
+            }
+        }
+
+        function setCell(row, column, marker) {
+            // Returns false if cell couldn't be set
+
+            if (row < 3 && column < 3) {
+                if (!board[row][column]) {
+                    board[row][column] = marker;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function resetBoard() {
+            for (let i = 0; i < 3; ++i) {
+                for (let j = 0; j < 3; ++j) {
+                    board[i][j] = "";
+                }
+            }
+        }
+
+        return {
+            getCell,
+            setCell,
+            resetBoard
+        };
+    })();
